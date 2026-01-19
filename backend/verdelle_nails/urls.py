8bf477293d2f59f10a1872806dff44f7d1c9cb83
Daniel Edgar <123147352+danieledgar@users.noreply.github.com>
2026-01-19 11:15:15 +0300

    Add redirect for root URL to frontend
    Add a redirect to the frontend URL for the root path.

--- backend/verdelle_nails/urls.py
+++ backend/verdelle_nails/urls.py
@@ -5,8 +5,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url=settings.FRONTEND_URL)),
+
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
